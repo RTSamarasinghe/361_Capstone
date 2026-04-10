5fdf377190bde1ece7fb764bc930533f1935de3b
eramaekers3 <eramaekers3@huskers.unl.edu>
2026-04-10 11:48:26 -0500

    Implemented CartItemEngine and CartItemManager

--- Engines/ICartItemEngine.cs
+++ Engines/ICartItemEngine.cs
@@ -1,6 +1,11 @@
-﻿
+﻿using DataContracts;
+
 public interface ICartItemEngine
 {
-
+    int AddCartItem(int cartId, int productId, int quantity);
+    CartItem GetCartItem(int id);
+    List<CartItem> GetCartItemsByCart(int cartId);
+    void UpdateCartItemQuantity(int id, int quantity);
+    void DeleteCartItem(int id);
+    void DeleteAllCartItems(int cartId);
 }
-
--- Managers/ICartItemManager.cs
+++ Managers/ICartItemManager.cs
@@ -5,6 +5,7 @@ public interface ICartItemManager
     int AddCartItem(int cartId, int productId, int quantity);
     CartItem GetCartItem(int id);
     List<CartItem> GetCartItemsByCart(int cartId);
+    void UpdateCartItemQuantity(int id, int quantity);
     void DeleteCartItem(int id);
     void DeleteAllCartItems(int cartId);
 }
