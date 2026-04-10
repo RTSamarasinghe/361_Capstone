b051ef979e4c133921b01b7308433673cf42f363
eramaekers3 <eramaekers3@huskers.unl.edu>
2026-04-10 12:14:47 -0500

    SaleCategory and SaleItem Engine Interfaces and Managers

--- Engines/ISaleItemEngine.cs
+++ Engines/ISaleItemEngine.cs
@@ -1,5 +1,11 @@
-﻿
+﻿using DataContracts;
+
 public interface ISaleItemEngine
 {
-
+    int AddSaleItem(int saleId, int productId);
+    SaleItem GetSaleItem(int id);
+    List<SaleItem> GetSaleItemsBySale(int saleId);
+    List<SaleItem> GetSaleItemsByProduct(int productId);
+    void DeleteSaleItem(int id);
+    void DeleteAllSaleItems(int saleId);
 }
--- Managers/ISaleCategoryManager.cs
+++ Managers/ISaleCategoryManager.cs
@@ -1,7 +1,11 @@
 using DataContracts;
 
-namespace Managers;
-
 public interface ISaleCategoryManager
 {
+    int AddSaleCategory(int saleId, int categoryId);
+    SaleCategory GetSaleCategory(int id);
+    List<SaleCategory> GetSaleCategoriesBySale(int saleId);
+    List<SaleCategory> GetSaleCategoriesByCategory(int categoryId);
+    void DeleteSaleCategory(int id);
+    void DeleteAllSaleCategories(int saleId);
 }
