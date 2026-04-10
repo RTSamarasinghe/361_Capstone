611913530ea0a24a23017d6f584998678aac4fc3
eramaekers3 <eramaekers3@huskers.unl.edu>
2026-04-10 11:56:11 -0500

    AddressEngine and AddressMaanger

--- Engines/IAddressEngine.cs
+++ Engines/IAddressEngine.cs
@@ -1,5 +1,11 @@
-﻿
+﻿using DataContracts;
+
 public interface IAddressEngine
 {
-
+    int AddAddress(int customerId, string street, string city, string state, string postalCode, string country);
+    Address GetAddress(int id);
+    List<Address> GetAddressesByCustomer(int customerId);
+    void UpdateAddress(int id, string street, string city, string state, string postalCode, string country);
+    void DeleteAddress(int id);
+    void DeleteAllAddresses(int customerId);
 }
