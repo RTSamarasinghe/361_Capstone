bdf94e599a619e9f63cf10525bc620b2fbd0239d
Liam Makela <liammakela06@gmail.com>
2026-04-17 11:58:53 -0500

    Added Pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,9 @@ import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
 import Home from "./pages/Home.tsx";
 import MainLayout from "./pages/MainLayout.tsx";
+import Cart from "./pages/Cart.tsx";
+import Checkout from "./pages/Checkout.tsx";
+import Product from "./pages/Product.tsx";
 
 export default function App() {
   return (
@@ -10,6 +13,9 @@ export default function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/Checkout" element={<Checkout />} />
+          <Route path="/Product" element={<Product />} />
         </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
