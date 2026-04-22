f5e2ac57beb31f87808da71230268fa631417401
pkorlacki <142928702+pkorlacki@users.noreply.github.com>
2026-04-22 12:24:06 -0500

    Added Checkout Manager

---
+++ Managers/ICheckoutManager.cs
@@ -0,0 +1,8 @@
+﻿using DataContracts;
+
+public interface ICheckoutManager
+{
+	void ConvertCartToOrder(int customerId, int shippingAddressId, int billingAddressId, int paymentMethodId);
+	void PayForOrder(int orderId, int paymentMethodId);
+}
+
