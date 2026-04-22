33ceae286ce0897aca7c5da72e743519ab94a1a9
pkorlacki <142928702+pkorlacki@users.noreply.github.com>
2026-04-21 22:02:18 -0500

    Added CheckoutEngine, not yet done implementing

---
+++ Engines/ICheckoutEngine.cs
@@ -0,0 +1,7 @@
+﻿using DataContracts;
+
+public interface ICheckoutEngine
+{
+	void ConvertCartToOrder(int customerId, int shippingAddressId, int billingAddressId, int paymentMethodId);
+	void PayForOrder(int customerId, int orderId, int paymentMethodId);
+}
