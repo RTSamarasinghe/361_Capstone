3d2e765498cb31943fc6252140ab8ae6bd154ef8
Liam Makela <liammakela06@gmail.com>
2026-04-22 12:19:55 -0500

    Configured Images to Database

--- Frontend/src/pages/Product.tsx
+++ Frontend/src/pages/Product.tsx
@@ -20,8 +20,7 @@ export default function Product() {
       {/* Image */}
       <div className="bg-white rounded-2xl shadow-md border border-gray-200 overflow-hidden">
         <img
-          //src={`http://localhost:5208/Images/${product.imageUrl}`}
-          src={product.imageUrl}
+          src={`http://localhost:5208/${product.imageUrl}`}
           alt={product.name}
           className="w-full h-full object-cover"
         />
